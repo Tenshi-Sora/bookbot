89261332d6f1d403ba0775af3748a735cb9c9f8a
Tenshi-Sora <76926133+Tenshi-Sora@users.noreply.github.com>
2025-02-14 21:00:10 -0500

    added a word count function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-with open("books/frankenstein.txt") as f:
+with open("books/frankenstein.txt") as f: # returns the frankenstein book as a string
     file_contents = f.read()
-print(file_contents)
+
+file_contents_word_count = len(file_contents.split()) # splits the singular string by word then gives the length/count of them
+print(file_contents_word_count)
